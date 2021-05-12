4054719aa66428a646d5a8df68f51b8bd431fdbe
Abdullah Alhariri <alhariri.abdu.2018@gmail.com>
2021-05-12 02:21:23 +0200

    ejs

--- server.js
+++ server.js
@@ -1,8 +1,23 @@
 const express = require("express");
+const dotenv = require("dotenv").config();
+const fs = require("fs");
+
+const publicKey = dotenv.parsed.STRIPE_PUBLIC_KEY;
+const secretKey = dotenv.parsed.STRIPE_SECRET_KEY;
 
 const app = express();
 
 app.set("view engine", "ejs");
-app.use(express.static("public"));
+app.use(express.static("public")); // This sets the public folder as our static files.
+
+app.get("/store", (req, res) => {
+  fs.readFile("item.json", (err, data) => {
+    if (err) res.status(500).end();
+    else
+      res.render("store.ejs", {
+        items: JSON.parse(data),
+      });
+  });
+});
 
 app.listen(3000);
